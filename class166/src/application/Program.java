4cbbb0be21fe0328ca33e50856a1eacc14e28b90
Derek Diniz <drk.diniz@gmail.com>
2024-06-26 18:41:14 -0300

    add class

--- class166/src/application/Program.java
+++ class166/src/application/Program.java
@@ -1,10 +1,59 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("Tax payer #%d data:\n", i);
+			System.out.print("Individual or Company (i/c)? ");
+			String ic = sc.next();
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Annual Income: ");
+			double annualIncome = sc.nextDouble();
+			
+			if (ic.equalsIgnoreCase("i")) {
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, annualIncome, healthExpenditures));
+			}
+			else {
+				System.out.print("Number of Eemployees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, annualIncome, numberOfEmployees));
+			}
+		}
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer payer : list) {
+			System.out.printf("%s: $ %.2f\n", payer.getName(), payer.getAnnualIncome());
+		}
+		double sum = 0.0;
+		for (TaxPayer payer : list) {
+			sum += payer.getAnnualIncome();
+		}
+		System.out.println();
+		System.out.println("TOTAL TAXES: $ " + sum);
 
+		
+		sc.close();
 	}
 
 }
